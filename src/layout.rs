58a5ad60ca3be38709b7f980e9ac9db33fcda651
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-06-15 17:42:59 -0400

    Change the error handling

---
+++ src/layout.rs
@@ -0,0 +1,5 @@
+use Result;
+
+pub fn generate(_core_area: f64, _l3_area: f64, _cores: usize) -> Result<()> {
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@
 extern crate arguments;
 extern crate sqlite;
 
-use arguments::Arguments;
+mod layout;
+
 use sqlite::Database;
 use std::fmt::Display;
 use std::path::Path;
@@ -18,60 +19,75 @@ Usage: layer [options]
 Options:
     --database PATH          SQLite3 database (required).
     --table    NAME          Table to look for area measurements (required).
+    --cores    NUMBER        Number of cores (required).
 
     --help                   Display this message.
 ";
 
 macro_rules! die(
-    ($($arg:tt)*) => (fail(format!($($arg)*)));
+    ($($arg:tt)*) => (raise!(format!($($arg)*)));
 );
 
 macro_rules! ok(
     ($result:expr) => (match $result {
         Ok(result) => result,
-        Err(error) => fail(error),
+        Err(error) => raise!(error),
     });
 );
 
-macro_rules! print_error(
-    ($($arg:tt)*) => ({
-        use std::io::Write;
-        ::std::io::stderr().write_fmt(format_args!($($arg)*)).unwrap_or(())
-    });
+macro_rules! raise(
+    ($error:expr) => (return Err(Box::new($error)));
 );
+
+macro_rules! usage(
+    () => (usage());
+);
+
+pub type Result<T> = std::result::Result<T, Box<Display>>;
 
 fn main() {
-    let Arguments { options, .. } = ok!(arguments::parse(env::args()));
-
-    let database = match options.get_ref::<String>("database") {
-        Some(database) => ok!(sqlite::open(&Path::new(database))),
-        _ => usage(USAGE),
-    };
-    let (core, l3) = match options.get_ref::<String>("table") {
-        Some(table) => (find(&database, table, CORE_LIKE), find(&database, table, L3_LIKE)),
-        _ => usage(USAGE),
-    };
-    println!("Core: {}", core);
-    println!("L3: {}", l3);
+    start().unwrap_or_else(|error| fail(&*error));
 }
 
-fn find(database: &Database, table: &str, like: &str) -> f64 {
+fn start() -> Result<()> {
+    let arguments = ok!(arguments::parse(env::args()));
+
+    let cores = match arguments.get::<usize>("cores") {
+        Some(cores) => cores,
+        _ => usage!(),
+    };
+    let database = match arguments.get::<String>("database") {
+        Some(ref database) => ok!(sqlite::open(&Path::new(database))),
+        _ => usage!(),
+    };
+    let (core_area, l3_area) = match arguments.get::<String>("table") {
+        Some(ref table) => {
+            (ok!(find(&database, table, CORE_LIKE)), ok!(find(&database, table, L3_LIKE)))
+        },
+        _ => usage!(),
+    };
+
+    layout::generate(core_area, l3_area, cores)
+}
+
+fn find(database: &Database, table: &str, like: &str) -> Result<f64> {
     use sqlite::State;
     let mut statement = ok!(database.prepare(&format!(
         "SELECT name, value FROM `{}` WHERE name LIKE '{}' LIMIT 1;", table, like,
     )));
-    match ok!(statement.step()) {
+    Ok(match ok!(statement.step()) {
         State::Row => ok!(statement.column::<f64>(0 + 1)),
-        _ => die!("failed to find a required value in the table"),
-    }
+        _ => raise!("failed to find a required value in the table"),
+    })
 }
 
-fn fail<T: Display>(error: T) -> ! {
-    print_error!("Error: {}.\n", error);
+fn fail<E: Display>(error: E) -> ! {
+    use std::io::{stderr, Write};
+    stderr().write_fmt(format_args!("Error: {}.\n", error)).unwrap_or(());
     process::exit(1);
 }
 
-fn usage(message: &str) -> ! {
-    print_error!("{}\n", message.trim());
-    process::exit(0);
+fn usage() -> ! {
+    println!("{}", USAGE);
+    process::exit(1);
 }
