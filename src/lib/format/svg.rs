9cc5fac750d7e2bd74cbb9cd87c4ef6bef7882ee
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-06-20 08:56:19 +0200

    Refactor the SVG formatter

--- src/lib/format/svg.rs
+++ src/lib/format/svg.rs
@@ -3,43 +3,18 @@ use std::io::Write;
 use Result;
 use layout::Element;
 
-/// The SVG format.
-pub struct SVG;
+const MARGIN: f64 = 5.0;
+const WIDTH: f64 = 800.0;
 
-impl super::Format for SVG {
-    fn write(&self, elements: &[Element], writer: &mut Write) -> Result<()> {
-        use std::f64::{INFINITY, NEG_INFINITY};
+macro_rules! element_template(
+    () => (
+r#"    <rect x="{x:.0}" y="{y:.0}" width="{width:.0}" height="{height:.0}" id="{id}" />
+"#
+    );
+);
 
-        let (mut min_x, mut min_y) = (INFINITY, INFINITY);
-        let (mut max_x, mut max_y) = (NEG_INFINITY, NEG_INFINITY);
-        for &Element { position: (x, y), dimension: (width, height), .. } in elements {
-            min_x = min_x.min(x);
-            min_y = min_y.min(y);
-            max_x = max_x.max(x + width);
-            max_y = max_y.max(y + height);
-        }
-
-        const MARGIN: f64 = 5.0;
-        const WIDTH: f64 = 800.0;
-
-        let scale = WIDTH / (max_x - min_x);
-        let height = (max_y - min_y) * scale;
-
-        let mut body = String::new();
-        for &Element { ref name, position: (x, y), dimension: (width, height) } in elements {
-            body.push_str(&format!(
-                "    <rect x=\"{:.0}\" \
-                           y=\"{:.0}\" \
-                           width=\"{:.0}\" \
-                           height=\"{:.0}\" \
-                           id=\"{}\" />\n",
-                MARGIN + scale * (x - min_x), MARGIN + scale * (y - min_y),
-                scale * width, scale * height, name,
-            ));
-        }
-
-        macro_rules! template(
-            () => (
+macro_rules! document_template(
+    () => (
 r#"<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="{width:.0}" height="{height:.0}">
   <style type="text/css">
     rect {{
@@ -59,12 +34,42 @@ r#"<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="{width:.0}" heig
   </g>
 </svg>
 "#
-            );
-        );
+    );
+);
 
-        ok!(writer.write_all(format!(template!(), width = WIDTH + 2.0 * MARGIN,
-                                     height = height + 2.0 * MARGIN,
-                                     body = body.trim()).as_bytes()));
+/// The SVG format.
+pub struct SVG;
+
+impl super::Format for SVG {
+    fn write(&self, elements: &[Element], writer: &mut Write) -> Result<()> {
+        use std::f64::{INFINITY, NEG_INFINITY};
+
+        let (mut min_x, mut min_y) = (INFINITY, INFINITY);
+        let (mut max_x, mut max_y) = (NEG_INFINITY, NEG_INFINITY);
+        for &Element { position: (x, y), dimension: (width, height), .. } in elements {
+            min_x = min_x.min(x);
+            min_y = min_y.min(y);
+            max_x = max_x.max(x + width);
+            max_y = max_y.max(y + height);
+        }
+
+        let scale = WIDTH / (max_x - min_x);
+        let height = (max_y - min_y) * scale;
+
+        let mut body = String::new();
+        for &Element { ref name, position: (x, y), dimension: (width, height) } in elements {
+            body.push_str(&format!(element_template!(),
+                                   id = name,
+                                   x = MARGIN + scale * (x - min_x),
+                                   y = MARGIN + scale * (y - min_y),
+                                   width = scale * width,
+                                   height = scale * height));
+        }
+
+        ok!(writer.write_fmt(format_args!(document_template!(),
+                                          width = WIDTH + 2.0 * MARGIN,
+                                          height = height + 2.0 * MARGIN,
+                                          body = body.trim())));
 
         Ok(())
     }
